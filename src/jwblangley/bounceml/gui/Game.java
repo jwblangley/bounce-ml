423a6a1fd52a9c6365204df193a713706e176aa0
James Langley <jwblangley@gmail.com>
2020-06-14 18:46:41 +0100

    Implement basic bouncing

--- src/jwblangley/bounceml/gui/Game.java
+++ src/jwblangley/bounceml/gui/Game.java
@@ -5,53 +5,83 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class Game extends Application {
 
-  public static int WIDTH = 300;
-  public static int HEIGHT = 500;
+  public static final int WIDTH = 300;
+  public static final int HEIGHT = 500;
+
+  public static final double GRAVITY = 0.5d;
+  public static final double JUMP_POWER = 7d;
+  public static final int MILLIS_BETWEEN_JUMPS = 150;
+
+  private Circle sprite;
+  private double fallVelocity = 0;
 
   @Override
   public void start(Stage stage) throws Exception {
     stage.setTitle("Bounce");
 
     Group root = new Group();
-    Scene scene = new Scene(root);
+    Scene scene = new Scene(root, WIDTH, HEIGHT);
     stage.setScene(scene);
 
-    Canvas canvas = new Canvas(WIDTH, HEIGHT);
-    root.getChildren().add(canvas);
+    sprite = new Circle(WIDTH / 2d, HEIGHT / 2d, 12.5, Color.BLACK);
+    root.getChildren().add(sprite);
 
-    GraphicsContext gc = canvas.getGraphicsContext2D();
-
-    AtomicReference<Boolean> show = new AtomicReference<>(false);
+    AtomicReference<Boolean> jump = new AtomicReference<>(false);
+    AtomicReference<Boolean> canJump = new AtomicReference<>(true);
     scene.setOnKeyPressed(event -> {
       if (event.getCode() == KeyCode.SPACE) {
-        show.set(true);
-      }
-    });
-    scene.setOnKeyReleased(event -> {
-      if (event.getCode() == KeyCode.SPACE) {
-        show.set(false);
+        requestJump(jump, canJump);
       }
     });
 
     new AnimationTimer() {
       @Override
       public void handle(long now) {
-        gc.clearRect(0,0,WIDTH, HEIGHT);
-        if (show.get()) {
-          gc.setFill(Color.BLACK);
-          gc.fillOval(100, 100, 25, 25);
+        fallVelocity += GRAVITY;
+        if (jump.getAndUpdate(a -> a)) {
+          jump.set(false);
+          fallVelocity = -1 * JUMP_POWER;
         }
+
+        sprite.setCenterY(clamp(sprite.getCenterY() + fallVelocity, sprite.getRadius(),
+            HEIGHT - sprite.getRadius()));
       }
     }.start();
 
     stage.show();
+  }
+
+  public double getFallVelocity() {
+    return fallVelocity;
+  }
+
+  private double getSpiteY() {
+    return sprite.getCenterY();
+  }
+
+  public void requestJump(AtomicReference<Boolean> jump, AtomicReference<Boolean> canJump) {
+    if (canJump.getAndUpdate(a -> a)) {
+      jump.compareAndSet(false, true);
+      canJump.compareAndSet(true, false);
+      new Thread(() -> {
+        try {
+          Thread.sleep(MILLIS_BETWEEN_JUMPS);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        canJump.compareAndSet(false, true);
+      }).start();
+    }
+  }
+
+  private static double clamp(double value, double min, double max) {
+    return Math.max(min, Math.min(max, value));
   }
 }
